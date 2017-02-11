daee5ba66bd99cdfeb4280e47a19edc18b9aa4f2
Nate Meyers <natedaswas@gmail.com>
2017-02-10 21:21:58 -0700

    completed early stage working models of post and category for blog

--- home/models.py
+++ home/models.py
@@ -8,7 +8,21 @@ class Post(models.Model):
     published = models.DateTimeField()
     slug = models.SlugField(max_length=100, unique=True)
 	category = models.ForeignKey('blog.Category')
+	
+	def __str__(self):
+		return self.title
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.title)
+		super(Post, self).save(*args, **kwargs)
 
 class Category(models.Model):
 	title = models.CharField(max_length=100, unique=True)
 	slug = models.SlugField(max_length=100, unique=True)
+
+	def __str__(self):
+		return self.title
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.title)
+		super(Category, self).save(*args, **kwargs)
