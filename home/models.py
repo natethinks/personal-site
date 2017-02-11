225eabf5fa3e3d7972996d7fb7c12655bb70652b
Nate Meyers <natedaswas@gmail.com>
2017-02-10 21:16:16 -0700

    added fields for category and blog post in models.py

--- home/models.py
+++ home/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, unique=True)
     author = models.CharField(max_length=50)
     content = models.TextField()
     published = models.DateTimeField()
-    slug = models.SlugField()
+    slug = models.SlugField(max_length=100, unique=True)
+	category = models.ForeignKey('blog.Category')
+
+class Category(models.Model):
+	title = models.CharField(max_length=100, unique=True)
+	slug = models.SlugField(max_length=100, unique=True)
