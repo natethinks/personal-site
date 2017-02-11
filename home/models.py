38e92b3cf9c98cd6bab4b42a5b49a07f9e73e060
Nate Meyers <natedaswas@gmail.com>
2017-02-10 21:27:14 -0700

    incorrect naming in models.py creating an error, fixed

--- home/models.py
+++ home/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=50, unique=True)
-    author = models.CharField(max_length=50)
-    content = models.TextField()
-    published = models.DateTimeField()
-    slug = models.SlugField(max_length=100, unique=True)
-	category = models.ForeignKey('blog.Category')
+	title = models.CharField(max_length=50, unique=True)
+	author = models.CharField(max_length=50)
+	content = models.TextField()
+	published = models.DateTimeField()
+	slug = models.SlugField(max_length=100, unique=True)
+	category = models.ForeignKey('home.Category')
 	
 	def __str__(self):
 		return self.title
