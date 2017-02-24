5a9d236224fcb0d3f6929197d7f3b8d41e785e83
Nate Meyers <natedaswas@gmail.com>
2017-02-23 18:33:20 -0700

    Added models for about and project section

--- home/models.py
+++ home/models.py
@@ -27,3 +27,33 @@ class Category(models.Model):
 	def save(self, *args, **kwargs):
 		self.slug = slugify(self.title)
 		super(Category, self).save(*args, **kwargs)
+
+class About(models.Model):
+	title = models.CharField(max_length=100, unique=True)
+	slug = models.SlugField(max_length=100, unique=True, blank=True)
+	image_1 = models.URLField(blank=True)
+	image_2 = models.URLField(blank=True)
+	image_3 = models.URLField(blank=True)
+	content = models.TextField()
+	
+	def __str__(self):
+		return self.title
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.title)
+		super(About, self).save(*args, **kwargs)
+
+class Project(models.Model):
+	title = models.CharField(max_length=100, unique=True)
+	slug = models.SlugField(max_length=100, unique=True, blank=True)
+	image_1 = models.URLField(blank=True)
+	image_2 = models.URLField(blank=True)
+	image_3 = models.URLField(blank=True)
+	content = models.TextField()
+
+	def __str__(self):
+		return self.title
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.title)
+		super(Project, self).save(*args, **kwargs)
