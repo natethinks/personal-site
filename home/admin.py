a3bd04abcdc0c14bc0839544210e62e053d2f913
Nate Meyers <natedaswas@gmail.com>
2017-02-28 16:19:45 -0700

    changed about view to list view and started building db call

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Post, Category
+from .models import Post, Category, About, Project
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(About)
+admin.site.register(Project)
--- home/views.py
+++ home/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from django.views.generic.detail import DetailView
-from home.models import Post
+from home.models import Post, About
 # Create your views here.
 
-class AboutView(TemplateView):
+class AboutView(ListView):
 	template_name = "home/about.html"
+	model = About
+
+	def get_context_data(self, **kwargs):
+		context = dict()
+		all_abouts = About.objects.all()
+		context['about_list'] = all_abouts
+		return context
 
 class HomeView(TemplateView):
 	template_name = "home/home.html"
